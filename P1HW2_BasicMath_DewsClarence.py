b462b52956f6d534a423188ae251b299b9d65ec3
dewsclarence <71237101+dewsclarence@users.noreply.github.com>
2020-09-13 20:38:14 -0400

    Add files via upload

---
+++ P1HW2_BasicMath_DewsClarence.py
@@ -0,0 +1,27 @@
+    # Using IDLE Math
+    # 9/13/2020
+    # CTI-110 P1HW2 - Basic Math
+    # Clarence Dews
+print("Enter number 1-10:", end=' ') 
+var1 = int(input())             # User input number 1-10
+print("Enter number 11-20:", end=' ')
+var2 = int(input())             # User input number 11-20
+print("First number entered: ",var1)  # Shows first number entered
+print("Second number entered: ",var2) # Shows second number entered
+var3 = var1 + var2    #Adds var1 and var2
+var4 = var1 * var2    #Multiplies var1 and var2 
+
+print("Sum of both numbers: ",var3)   #Displays results of addition
+print("Result of mutiplying both numbers: ",var4) #Displays results of multiplication
+
+
+#Start
+#Input number 1-10
+#Input number 11-20
+#Displays var1
+#Displays var2
+#Calculates addition of var1 and var2
+#Calculates multiplication of var1 and var2
+#Displays var3
+#Displays var4
+#End
